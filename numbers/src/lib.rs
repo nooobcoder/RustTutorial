12147f4ee3b6368ddd5b6302e88c1fcee005402d
Ankur Paul <suryashi2013@gmail.com>
2021-09-03 17:06:03 +0000

    Vectors and the copy trait

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -1,10 +1,11 @@
 pub fn print() {
-    let numbers = [1, 2, 3, 4, 5];
-    output_sequence(numbers)
+    let numbers = vec![1, 2, 3, 4, 5];
+    output_sequence(numbers);
+    output_sequence(numbers); // This throws error because, vectors do not inherit the Copy trait
 }
 
-fn output_sequence(numbers: [u8; 5]) {
-    for n in numbers.iter() {
+fn output_sequence(numbers: Vec<u8>) {
+    for n in numbers {
         print!("{}, ", n)
     }
 }
