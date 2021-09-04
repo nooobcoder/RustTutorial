5728037657b31193855ab2d3a4a0a5b719581b1b
Ankur Paul <suryashi2013@gmail.com>
2021-09-04 15:02:48 +0000

    Generating vector from a limit from user

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn print() {
-    let numbers = vec![1, 2, 3, 4, 5];
-    output_sequence(&numbers);
-
-    let numbers2 = [1, 2, 3, 4, 5];
-    output_sequence(&numbers2);
+pub fn print(limit: u8) {
+    let numbers = generate_sequence(limit);
+    print_sequence(&numbers);
 }
 
-fn output_sequence(numbers: &[u8]) {
-    for n in numbers {
+fn generate_sequence(limit: u8) -> Vec<u8> {
+    let mut numbers = Vec::new();
+
+    for n in 1..=limit {
+        numbers.push(n);
+    }
+
+    return numbers;
+}
+
+fn print_sequence(numbers: &[u8]) {
+    for n in numbers.iter() {
         print!("{}, ", n)
     }
-    println!()
 }
