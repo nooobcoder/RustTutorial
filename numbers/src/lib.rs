d03c511300f2db3555b8bd5d040c0b18e5991197
Ankur Paul <suryashi2013@gmail.com>
2021-09-04 03:01:56 +0000

    The slice data type

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn print() {
     let numbers = vec![1, 2, 3, 4, 5];
-    output_sequence(numbers);
-    output_sequence(numbers); // This throws error because, vectors do not inherit the Copy trait
+    output_sequence(&numbers);
+
+    let numbers2 = [1, 2, 3, 4, 5];
+    output_sequence(&numbers2);
 }
 
-fn output_sequence(numbers: Vec<u8>) {
+fn output_sequence(numbers: &[u8]) {
     for n in numbers {
         print!("{}, ", n)
     }
+    println!()
 }
