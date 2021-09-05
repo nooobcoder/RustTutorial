40ebf92e56ca9dd7f28ffa2afdde4cca56ae733b
Ankur Paul <suryashi2013@gmail.com>
2021-09-05 13:55:10 +0000

    Created first unit test

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -3,6 +3,13 @@ pub fn print(limit: u8) {
     print_sequence(&numbers);
 }
 
+// Test
+#[test]
+fn generate_sequence_should_work() {
+    let result = generate_sequence(3);
+    assert_eq!(result, &[1, 2, 3]);
+}
+
 fn generate_sequence(limit: u8) -> Vec<u8> {
     let mut numbers = Vec::new();
 
