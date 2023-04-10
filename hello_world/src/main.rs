0e7e00f4f6063010bd8493d32cbbbf7d744684a8
Ankur Paul <suryashi2013@gmail.com>
2023-04-10 08:51:26 +0000

    add structs demo

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -9,33 +9,19 @@ fn print_type_of<T>(_: &T) {
 }
 //#endregion
 
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
-
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file: File = File::open(filename).expect("File not found");
-    let buf: BufReader<File> = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
+#[derive(Debug)]
+struct Shuttle {
+    name: String,
+    crew_size: u8,
+    propellant: f64,
 }
 
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect::<Vec<String>>();
-    // Get the first and second arguments with error catching
-    let filename: &str = args.get(2).expect("No filename provided");
-    let search_str: &str = args.get(3).expect("No search string provided");
+    let vehicle: Shuttle = Shuttle {
+        name: String::from("Discovery"),
+        crew_size: 5,
+        propellant: 82372.2,
+    };
 
-    println!("Filename: {}\nSearch String: {}", filename, search_str);
-    let lines: Vec<String> = lines_from_file(filename);
-    for (index, line) in lines.iter().enumerate() {
-        if line == search_str {
-            println!("Found: {} at line index: #{}", search_str, index);
-        }
-    }
+    println!("{:?}", vehicle);
 }
